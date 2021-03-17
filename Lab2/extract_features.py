e488e8abf63032b1c02b1dabb8077b5d01f3199b
FernandoVN98 <38290736+FernandoVN98@users.noreply.github.com>
2021-03-17 10:44:50 +0100

    mas features

--- Lab2/extract_features.py
+++ Lab2/extract_features.py
@@ -19,43 +19,49 @@ def extract_features(s):
      [ "form=aspirin",   "suf4=irin", "next=,", "prev=," ],
      ...]
      """
-
     lemmatizer = WordNetLemmatizer()
 
     listFeatureVectors = []
-    for sentence in s:
+    for i in range(len(s)):
         FeatureVector = []
-        FeatureVector.append("form=" + sentence[0])
-        #FeatureVector.append("suf5=" + sentence[0][-5:])
-        #FeatureVector.append("suf6=" + sentence[0][-6:])
-        #FeatureVector.append("pref4=" + sentence[0][:4])
-        #FeatureVector.append("lemma=" + lemmatizer.lemmatize(sentence[0]))
+        FeatureVector.append("form=" + s[i][0])
+        FeatureVector.append("suf5=" + s[i][0][-5:])
+        #FeatureVector.append("suf6=" + s[i][0][-6:])
+        FeatureVector.append("pref4=" + s[i][0][:4])
+        #FeatureVector.append("lemma=" + lemmatizer.lemmatize(s[i][0]))
 
-        '''if sentence[0][-1] == "s":
+        if s[i][0][-1] == "s":
             FeatureVector.append("endsWithS")
         else:
             FeatureVector.append("None")
 
-        if sentence[0].isupper():
+        '''if s[i][0].isupper():
             FeatureVector.append("isCapitalized")
         else:
-            FeatureVector.append("None")
-
-        if sentence[1] == 0:
+            FeatureVector.append("None")'''
+        if s[i][1] == 0:
             FeatureVector.append("prev=_BoS_")
         else:
             FeatureVector.append("prev=" + previous)
-        previous = sentence[0]
-
-        if sentence[0] in string.punctuation:
+        previous = s[i][0]
+        '''if i<(len(s)-1):
+            FeatureVector.append("next="+s[i+1][0])
+            FeatureVector.append("nextsuf5=" + s[i+1][0][-5:])
+            #FeatureVector.append("nextpref4=" + s[i+1][0][:4])
+        else:
+            FeatureVector.append("next=_EoS_")
+            FeatureVector.append("None")
+            #FeatureVector.append("None")
+'''
+        '''if s[i][0] in string.punctuation:
             FeatureVector.append("punct")
         else:
             FeatureVector.append("None")
-
-        if any(map(str.isdigit, sentence[0])):
+        '''
+        '''if any(map(str.isdigit, s[i][0])):
             FeatureVector.append("hasDigits")
         else:
-            FeatureVector.append("None")'''
-
+            FeatureVector.append("None")
+'''
         listFeatureVectors.append(FeatureVector)
     return listFeatureVectors
